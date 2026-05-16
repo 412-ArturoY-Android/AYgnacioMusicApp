b18e0008401e66aed57b5358ee842cf833cbe4a3
YgnacioDelRio <arturoy.delrio@gmail.com>
2026-05-15 20:18:15 -0600

    MainActivity

--- app/src/main/java/com/example/aygnaciomusicapp/MainActivity.kt
+++ app/src/main/java/com/example/aygnaciomusicapp/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aygnaciomusicapp.ui.theme.AYgnacioMusicAppTheme
+import androidx.compose.material3.Surface
+import com.example.aygnaciomusicapp.navigation.MusicAppNavigation
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AYgnacioMusicAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    MusicAppNavigation()
                 }
             }
         }
